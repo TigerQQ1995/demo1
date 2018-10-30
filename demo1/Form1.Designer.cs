4f2f53d45d38cfc93818b8ba89f2def795f9aab3
HZW <r@DESKTOP-GPIUP35>
2018-10-30 09:40:43 +0800

    940

--- demo1/Form1.Designer.cs
+++ demo1/Form1.Designer.cs
@@ -28,13 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.L_x = new System.Windows.Forms.TextBox();
+            this.L_y = new System.Windows.Forms.TextBox();
+            this.L_z = new System.Windows.Forms.TextBox();
+            this.L_v = new System.Windows.Forms.TextBox();
+            this.close = new System.Windows.Forms.RadioButton();
+            this.open = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "movej";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(463, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox22
+            // 
+            this.textBox22.Location = new System.Drawing.Point(676, 270);
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(100, 25);
+            this.textBox22.TabIndex = 76;
+            this.textBox22.Text = "1";
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(560, 270);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(100, 25);
+            this.textBox21.TabIndex = 75;
+            this.textBox21.Text = "0";
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(454, 270);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(100, 25);
+            this.textBox20.TabIndex = 74;
+            this.textBox20.Text = "-1.5";
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(348, 270);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(100, 25);
+            this.textBox19.TabIndex = 73;
+            this.textBox19.Text = "0";
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(242, 270);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(100, 25);
+            this.textBox18.TabIndex = 72;
+            this.textBox18.Text = "-1.2";
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(128, 270);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(100, 25);
+            this.textBox17.TabIndex = 71;
+            this.textBox17.Text = "0";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(22, 270);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(100, 25);
+            this.textBox16.TabIndex = 70;
+            this.textBox16.Text = "0";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 77;
+            this.button5.Text = "line2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // L_x
+            // 
+            this.L_x.Location = new System.Drawing.Point(25, 155);
+            this.L_x.Name = "L_x";
+            this.L_x.Size = new System.Drawing.Size(100, 25);
+            this.L_x.TabIndex = 78;
+            this.L_x.Text = "-0.5";
+            // 
+            // L_y
+            // 
+            this.L_y.Location = new System.Drawing.Point(200, 155);
+            this.L_y.Name = "L_y";
+            this.L_y.Size = new System.Drawing.Size(100, 25);
+            this.L_y.TabIndex = 79;
+            this.L_y.Text = "-0.2";
+            // 
+            // L_z
+            // 
+            this.L_z.Location = new System.Drawing.Point(374, 155);
+            this.L_z.Name = "L_z";
+            this.L_z.Size = new System.Drawing.Size(100, 25);
+            this.L_z.TabIndex = 80;
+            this.L_z.Text = "0.55";
+            // 
+            // L_v
+            // 
+            this.L_v.Location = new System.Drawing.Point(561, 155);
+            this.L_v.Name = "L_v";
+            this.L_v.Size = new System.Drawing.Size(100, 25);
+            this.L_v.TabIndex = 81;
+            this.L_v.Text = "20";
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.Checked = true;
+            this.close.Location = new System.Drawing.Point(18, 33);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(68, 19);
+            this.close.TabIndex = 82;
+            this.close.TabStop = true;
+            this.close.Text = "close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.CheckedChanged += new System.EventHandler(this.close_CheckedChanged);
+            // 
+            // open
+            // 
+            this.open.AutoSize = true;
+            this.open.Location = new System.Drawing.Point(16, 68);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(60, 19);
+            this.open.TabIndex = 83;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.CheckedChanged += new System.EventHandler(this.open_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.open);
+            this.groupBox1.Controls.Add(this.close);
+            this.groupBox1.Location = new System.Drawing.Point(888, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(87, 96);
+            this.groupBox1.TabIndex = 84;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "JZstate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1182, 641);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.L_v);
+            this.Controls.Add(this.L_z);
+            this.Controls.Add(this.L_y);
+            this.Controls.Add(this.L_x);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox22);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox L_x;
+        private System.Windows.Forms.TextBox L_y;
+        private System.Windows.Forms.TextBox L_z;
+        private System.Windows.Forms.TextBox L_v;
+        private System.Windows.Forms.RadioButton close;
+        private System.Windows.Forms.RadioButton open;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- demo1/Form1.cs
+++ demo1/Form1.cs
@@ -7,14 +7,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
 
 namespace demo1
 {
     public partial class Form1 : Form
     {
+        aubocontrol.Aubo AuboArm = new aubocontrol.Aubo();
+
+        SerialPort port;
+
+        #region 窗口初始化以及JZ串口设置
         public Form1()
         {
             InitializeComponent();
+            this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+
+            if (port == null)
+            {
+                //COM4为Arduino使用的串口号，需根据实际情况调整
+                port = new SerialPort("COM4", 9600);
+                port.Open();
+            }
         }
+        #endregion
+
+        #region JZ初始化
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+        }
+
+        //点亮
+        private void rbOpen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.open.Checked)
+            {
+                PortWrite("0");
+            }
+        }
+
+        //熄灭
+        private void rbClose_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.close.Checked)
+            {
+                PortWrite("1");
+            }
+        }
+
+        //向串口输出命令字符
+        private void PortWrite(string message)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Write(message);
+                //port.WriteLine(message);
+            }
+        }
+        #endregion
+
+        #region 机器人开关态
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AuboArm.OpenRobot();
+
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AuboArm.botclose();
+        }
+        #endregion
+
+        #region movej
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread th_movej = new Thread(movej);
+            th_movej.IsBackground = true;
+            th_movej.Start();
+
+           
+        }//关节移动到
+        //j1~j6     弧度    取值范围  -3.14/2  ~3.14/2
+        //速度speed  取值范围  0~100
+        private void movej()
+        {
+            //movej
+            AuboArm.moveline(double.Parse(textBox16.Text), double.Parse(textBox17.Text), double.Parse(textBox18.Text),
+              double.Parse(textBox19.Text), double.Parse(textBox20.Text), double.Parse(textBox21.Text), double.Parse(textBox22.Text));
+        }
+        #endregion
+
+        #region line
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AuboArm.movelineto2(-0.067, -0.223, 0.88, 10);
+
+//[-3.14/2  ~3.14/2]//速度speed  取值范围  0~100
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Thread th_line = new Thread(line);
+            th_line.IsBackground = true;
+            th_line.Start();
+        }
+        private void line()
+        {
+            AuboArm.movelineto2(double.Parse(L_x.Text), double.Parse(L_y.Text), double.Parse(L_z.Text), double.Parse(L_v.Text));
+        }
+        #endregion 
+     
+
+        #region 控制夹抓开关
+        private void open_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread th_open = new Thread(openJZ);
+            th_open.IsBackground = true;
+            th_open.Start();
+        }
+        private void openJZ()
+        {
+            if (this.open.Checked)
+            {
+                PortWrite("1");
+            }
+        }
+        private void close_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread th_close = new Thread(closeJZ);
+            th_close.IsBackground = true;
+            th_close.Start();
+        }
+        private void closeJZ()
+        {
+            if (this.close.Checked)
+            {
+                PortWrite("0");
+            }
+        }
+        #endregion
     }
 }
